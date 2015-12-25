86b9042046c4286a1acc6c82493a426fc3e11389
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2015-12-25 13:07:55 +0000

    rework

--- master/database/datapump/postgres/run.sh
+++ master/database/datapump/postgres/run.sh
@@ -1,2 +1,43 @@
-echo ignore now
+#!/bin/bash
 
+P_CMD=$1
+P_SET=$2
+P_SCHEMASET="$3"
+
+if [ "$P_CMD" = "" ] || [ "$P_SET" = "" ] || [ "$P_SCHEMASET" = "" ]; then
+	echo invalid params. Exiting
+	exit 1
+fi
+
+function f_execute_start() {
+	echo EXPORT-STARTED > run.sh.log
+
+	if [ "$P_SET" = "meta" ]; then
+		nohup ./run-meta.sh "$P_SCHEMASET" >> run.sh.log 2>&1 &
+
+	elif [ "$P_SET" = "data" ]; then
+		nohup ./run-data.sh "$P_SCHEMASET" >> run.sh.log 2>&1 &
+	fi
+}
+
+function f_execute_status() {
+	if [ "`grep -c EXPORT-STARTED run.sh.log`" = "0" ]; then
+		echo UNKNOWN
+	fi
+
+	if [ "`grep -c EXPORT-FINISHED run.sh.log`" = "0" ]; then
+		echo RUNNING
+	fi
+
+	echo FINISHED
+}
+
+function f_execute_all() {
+	if [ "$CMD" = "start" ]; then
+		f_execute_start
+	elif [ "$CMD" = "status" ]; then
+		f_execute_status
+	fi
+}
+
+f_execute_all
