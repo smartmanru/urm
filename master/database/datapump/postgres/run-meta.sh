134b885b313a2e376c023cea7aa7dc31216f4fea
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2015-12-25 13:26:17 +0000

    rework

---
+++ master/database/datapump/postgres/run-meta.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+P_SCHEMASET="$1"
+
+function f_execute_one() {
+	P_SCHEMA=$1
+
+	echo "run pg_dump -s -f meta-$P_SCHEMA.dump -F c $P_SCHEMA ..."
+	pg_dump -s -f meta-$P_SCHEMA.dump -F c $P_SCHEMA > meta-$P_SCHEMA.dump.log 2>&1
+	local F_STATUS = $?
+
+	if [ "$F_STATUS" != "0" ]; then
+		echo pg_dump failed with status=$F_STATUS. Exiting
+		exit 1
+	fi
+}
+
+function f_execute_all() {
+	for schema in $P_SCHEMASET; do
+		f_execute_one $schema
+	done
+	echo EXPORT-FINISHED
+}
+
+f_execute_all
