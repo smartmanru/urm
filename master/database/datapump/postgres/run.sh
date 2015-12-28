e65b1c52d58d6bd217393d6c2a29cf8280d08cd3
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2015-12-28 15:10:35 +0000

    rework

--- master/database/datapump/postgres/run.sh
+++ master/database/datapump/postgres/run.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-P_CMD=$1
-P_SET=$2
-P_SCHEMA="$3"
+P_MODE=$1
+P_CMD=$2
+P_SET=$3
+P_SCHEMA="$4"
 
 if [ "$P_CMD" = "" ]; then
 	echo invalid params. Exiting
 	exit 1
 fi
 
-function f_execute_data() {
-	nohup ./run-meta.sh "$P_SCHEMA" >> run.sh.log 2>&1 &
+function f_execute_export_data() {
+	nohup ./run-export-meta.sh "$P_SCHEMA" >> run.sh.log 2>&1 &
 }
 
-function f_execute_meta() {
-	nohup ./run-data.sh "$P_SCHEMA" >> run.sh.log 2>&1 &
+function f_execute_export_meta() {
+	nohup ./run-export-data.sh "$P_SCHEMA" >> run.sh.log 2>&1 &
 }
 
-function f_execute_start() {
+function f_execute_import_data() {
+	nohup ./run-import-meta.sh "$P_SCHEMA" >> run.sh.log 2>&1 &
+}
+
+function f_execute_import_meta() {
+	nohup ./run-import-data.sh "$P_SCHEMA" >> run.sh.log 2>&1 &
+}
+
+function f_execute_export_start() {
 	if [ "$P_SET" = "" ] || [ "$P_SCHEMA" = "" ]; then
 		echo invalid params. Exiting
 		exit 1
@@ -26,14 +35,14 @@ function f_execute_start() {
 	echo EXPORT-STARTED > run.sh.log
 
 	if [ "$P_SET" = "meta" ]; then
-		f_execute_data
+		f_execute_export_data
 
 	elif [ "$P_SET" = "data" ]; then
-		f_execute_meta
+		f_execute_export_meta
 	fi
 }
 
-function f_execute_status() {
+function f_execute_export_status() {
 	if [ "`grep -c EXPORT-STARTED run.sh.log`" = "0" ]; then
 		echo UNKNOWN
 		return
@@ -52,13 +61,63 @@ function f_execute_status() {
 	echo FINISHED
 }
 
+function f_execute_import_status() {
+	if [ "`grep -c IMPORT-STARTED run.sh.log`" = "0" ]; then
+		echo UNKNOWN
+		return
+	fi
+
+	if [ "`pgrep -f pg_dump`" != "" ]; then
+		echo RUNNING
+		return
+	fi
+
+	if [ "`grep -c IMPORT-FINISHED run.sh.log`" = "0" ]; then
+		echo BROKEN
+		return
+	fi
+
+	echo FINISHED
+}
+
+function f_execute_import_start() {
+	if [ "$P_SET" = "" ] || [ "$P_SCHEMA" = "" ]; then
+		echo invalid params. Exiting
+		exit 1
+	fi
+
+	echo IMPORT-STARTED > run.sh.log
+
+	if [ "$P_SET" = "meta" ]; then
+		f_execute_import_data
+
+	elif [ "$P_SET" = "data" ]; then
+		f_execute_import_meta
+	fi
+}
+
 function f_execute_all() {
-	if [ "$P_CMD" = "start" ]; then
-		f_execute_start
-	elif [ "$P_CMD" = "status" ]; then
-		f_execute_status
+	if [ "$P_MODE" = "export" ]; then
+		if [ "$P_CMD" = "start" ]; then
+			f_execute_export_start
+		elif [ "$P_CMD" = "status" ]; then
+			f_execute_export_status
+		else
+			echo invalid command=$P_CMD
+		fi
+
+	elif [ "$P_MODE" = "import" ]; then
+		if [ "$P_CMD" = "start" ]; then
+			f_execute_import_start
+		elif [ "$P_CMD" = "status" ]; then
+			f_execute_import_status
+		else
+			echo invalid command=$P_CMD
+		fi
+
 	else
-		echo invalid command=$P_CMD
+		echo invalid mode=$P_MODE. Exiting
+		exit 1
 	fi
 }
 
