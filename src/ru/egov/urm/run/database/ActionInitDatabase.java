bc3fda1d87111a40bce4e9073df1389a0891ff31
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2015-12-28 10:45:18 +0000

    add initdb

---
+++ src/ru/egov/urm/run/database/ActionInitDatabase.java
@@ -0,0 +1,43 @@
+package ru.egov.urm.run.database;
+
+import ru.egov.urm.Common;
+import ru.egov.urm.meta.MetaEnvServer;
+import ru.egov.urm.run.ActionBase;
+import ru.egov.urm.storage.FileSet;
+import ru.egov.urm.storage.LocalFolder;
+import ru.egov.urm.storage.UrmStorage;
+
+public class ActionInitDatabase extends ActionBase {
+
+	MetaEnvServer server;
+
+	public ActionInitDatabase( ActionBase action , String stream , MetaEnvServer server ) {
+		super( action , stream );
+		this.server = server;
+	}
+
+	@Override protected boolean executeSimple() throws Exception {
+		DatabaseClient client = new DatabaseClient( server );
+		log( "initialize administrative database " + server.admSchema.DBNAME + " on database server " + server.NAME + " ..." );
+		if( !client.checkConnect( this ) )
+			exit( "unable to connect to administrative db" );
+
+		UrmStorage urm = artefactory.getUrmStorage();
+		LocalFolder urmScripts = urm.getInitScripts( this , server.DBMSTYPE );
+		
+		LocalFolder logs = artefactory.getWorkFolder( this , "initdb" );
+		FileSet files = urmScripts.getFileSet( this );
+		for( String file : Common.getSortedKeys( files.files ) )
+			executeInitScript( client , urmScripts , logs , file );
+		
+		return( true );
+	}
+	
+	private void executeInitScript( DatabaseClient client , LocalFolder scripts , LocalFolder logs , String file ) throws Exception {
+		log( "apply " + file + " ..." );
+		String logfile = file + ".out";
+		if( !client.applyScript( this , server.admSchema , scripts , file , logs , logfile ) )
+			exit( "unable to initialize database, see errors" );
+	}
+
+}
