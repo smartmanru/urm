06abf1b5b59e63f7f31394806abd2f8b4e29190c
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2015-12-28 16:03:39 +0000

    rework

--- master/database/datapump/postgres/run-import-data.sh
+++ master/database/datapump/postgres/run-import-data.sh
@@ -1,1 +1,55 @@
-echo not implemented
+#!/bin/bash
+
+P_SCHEMA="$1"
+
+. ./run.conf
+
+function f_execute_db() {
+	P_DBNAME=$1
+
+	echo load data schema=$P_SCHEMA dbname=$P_DBNAME ...
+
+	# get table set
+	F_TABLES=`cat tableset.txt | grep ^$P_DBNAME/ | cut -d "/" -f2 | tr "\n" " "`
+	F_TABLES=${F_TABLES% }
+	F_TABLES=${F_TABLES# }
+	if [ "$F_TABLES" = "" ]; then
+		echo invalid call due to empty tableset. Exiting
+		exit 1
+	fi
+
+	F_TABLEFILTER=
+	if [ "$F_TABLES" != "*" ]; then
+		echo load selected tables ...
+		for table in $F_TABLES; do
+			F_TABLEFILTER="$F_TABLEFILTER -t \"$table\""
+		done
+	else
+		echo load all schema tables ...
+	fi
+
+	F_CMD="pg_restore -a -c -j 4 --disable-triggers -d $P_DBNAME $F_TABLEFILTER ../data/data-$P_SCHEMA-all.dump"
+	echo "run: $F_CMD ..."
+	$F_CMD > ../log/data-$P_SCHEMA-all.dump.log 2>&1
+	F_STATUS=$?
+
+	if [ "$F_STATUS" != "0" ]; then
+		echo pg_dump failed with status=$F_STATUS. Exiting
+		exit 1
+	fi
+
+	echo EXPORT-FINISHED
+}
+
+function f_execute_all() {
+	# get schema name
+	local F_DBNAME=`echo "$CONF_MAPPING" | tr " " "\n" | grep ^$P_SCHEMA= | cut -d "=" -f2`
+	if [ "$F_DBNAME" = "" ]; then
+		echo unable to find schema=$P_SCHEMA in run.conf in CONF_MAPPING variable. Exiting
+		exit 1
+	fi
+
+	f_execute_db "$F_DBNAME"
+}
+
+f_execute_all
--- master/database/datapump/postgres/run-import-meta.sh
+++ master/database/datapump/postgres/run-import-meta.sh
@@ -1,1 +1,40 @@
-echo not implemented
+#!/bin/bash
+
+. ./run.conf
+
+function f_execute_one() {
+	P_SCHEMA=$1
+	P_DBNAME=$2
+
+	echo load meta schema=$P_SCHEMA dbname=$P_DBNAME ...
+
+	F_CMD="pg_restore -s -c -j 4 -d $P_DBNAME ../data/meta-$P_SCHEMA.dump"
+	echo "run: $F_CMD ..."
+	$F_CMD > ../log/meta-$P_SCHEMA.dump.log 2>&1
+	F_STATUS=$?
+
+	if [ "$F_STATUS" != "0" ]; then
+		echo pg_dump failed with status=$F_STATUS. Exiting
+		exit 1
+	fi
+}
+
+function f_execute_all() {
+	# get schema names
+	local F_SCHEMASET=`echo "$CONF_MAPPING" | tr " " "\n" | cut -d "=" -f1 | tr "\n" " "`
+	F_SCHEMASET="${F_SCHEMASET# }
+	F_SCHEMASET="${F_SCHEMASET% }
+	if [ "$F_SCHEMASET" = "" ]; then
+		echo unable to find schema set in run.conf in CONF_MAPPING variable. Exiting
+		exit 1
+	fi
+
+	local F_DBNAME
+	for schema in $F_SCHEMASET; do
+		local F_DBNAME=`echo "$CONF_MAPPING" | tr " " "\n" | grep ^$P_SCHEMA= | cut -d "=" -f2`
+		f_execute_one "$schema" "$F_DBNAME"
+	done
+	echo IMPORT-FINISHED
+}
+
+f_execute_all
