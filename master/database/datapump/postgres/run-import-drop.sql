a6fdd275990b6c71952c039d815a789f1e17e385
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2015-12-29 11:25:08 +0000

    rework

--- master/database/datapump/postgres/run-import-drop.sql
+++ master/database/datapump/postgres/run-import-drop.sql
@@ -3,7 +3,7 @@ DECLARE
 	sname RECORD;
 BEGIN
 	FOR sname IN SELECT schema_name FROM information_schema.schemata LOOP
-		if sname.schema_name NOT LIKE 'pg_%' then
+		if sname.schema_name NOT LIKE 'pg_%' and sname.schema_name <> 'information_schema' then
 			execute 'drop schema ' || sname.schema_name || ' cascade';
 		end if;
 	END LOOP;
