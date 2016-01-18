ecf16e5de95c8f8e67275c55a4e7214b738b5f46
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-01-18 08:43:43 +0000

    add using prod repository

--- src/ru/egov/urm/storage/DistRepository.java
+++ src/ru/egov/urm/storage/DistRepository.java
@@ -35,14 +35,14 @@ public class DistRepository {
 	public DistStorage getDistByLabel( ActionBase action , String RELEASELABEL ) throws Exception {
 		action.checkRequired( RELEASELABEL , "RELEASELABEL" );
 		
-		String RELEASEDIR = getReleaseDirByLabel( action , RELEASELABEL );
+		String RELEASEPATH = getReleasePathByLabel( action , RELEASELABEL );
 		
-		RemoteFolder distFolder = repoFolder.getSubFolder( action , RELEASEDIR );
+		RemoteFolder distFolder = repoFolder.getSubFolder( action , RELEASEPATH );
 		DistStorage storage = new DistStorage( artefactory , distFolder );
 		
 		// check release directory exists
 		if( !distFolder.checkExists( action ) )
-			action.exit( "unknown release=" + RELEASEDIR );
+			action.exit( "release does not exist at " + RELEASEPATH );
 		
 		storage.load( action );
 		return( storage );
@@ -51,14 +51,14 @@ public class DistRepository {
 	public DistStorage createDist( ActionBase action , String RELEASELABEL , VarBUILDMODE BUILDMODE ) throws Exception {
 		action.checkRequired( RELEASELABEL , "RELEASELABEL" );
 		
-		String RELEASEDIR = getReleaseDirByLabel( action , RELEASELABEL );
+		String RELEASEPATH = getReleasePathByLabel( action , RELEASELABEL );
 		
-		RemoteFolder distFolder = repoFolder.getSubFolder( action , RELEASEDIR );
+		RemoteFolder distFolder = repoFolder.getSubFolder( action , RELEASEPATH );
 		DistStorage storage = new DistStorage( artefactory , distFolder );
 		
 		// check release directory exists
 		if( distFolder.checkExists( action ) )
-			action.exit( "release " + RELEASEDIR + " already exists" );
+			action.exit( "release already exists at " + RELEASEPATH );
 
 		storage.create( action , BUILDMODE );
 		storage.load( action );
@@ -66,40 +66,40 @@ public class DistRepository {
 	}
 
 	public String getReleaseProdDir( ActionBase action ) throws Exception {
-		return( getReleaseDirByLabel( action , "prod" ) );
+		return( getReleasePathByLabel( action , "prod" ) );
 	}
 	
-	public String getReleaseDirByLabel( ActionBase action , String RELEASELABEL ) throws Exception {
+	private String getReleasePathByLabel( ActionBase action , String RELEASELABEL ) throws Exception {
 		action.checkRequired( RELEASELABEL , "RELEASELABEL" );
 		
-		String RELEASEDIR = "";
+		String RELEASEPATH = "";
 		if( RELEASELABEL.equals( "last" ) ) {
-			RELEASEDIR = meta.product.CONFIG_VERSION_LAST_FULL;
-			if( RELEASEDIR.isEmpty() )
+			RELEASEPATH = meta.product.CONFIG_VERSION_LAST_FULL;
+			if( RELEASEPATH.isEmpty() )
 				action.exit( "CONFIG_VERSION_LAST_FULL is not set in product.conf" );
+			
+			RELEASEPATH = "releases/" + RELEASEPATH;
 		}
 		else if( RELEASELABEL.equals( "next" ) ) {
-			RELEASEDIR = meta.product.CONFIG_VERSION_NEXT_FULL;
-			if( RELEASEDIR.isEmpty() )
+			RELEASEPATH = meta.product.CONFIG_VERSION_NEXT_FULL;
+			if( RELEASEPATH.isEmpty() )
 				action.exit( "CONFIG_VERSION_NEXT_FULL is not set in product.conf" );
+
+			RELEASEPATH = "releases/" + RELEASEPATH;
 		}
 		else if( RELEASELABEL.equals( "prod" ) ) {
-			RELEASEDIR = repoFolder.findOneTop( action , "*-prod" );
+			RELEASEPATH = "prod";
 
 			// check content
-			RELEASEDIR.trim();
-			if( RELEASEDIR.isEmpty() )
+			if( !repoFolder.checkFolderExists( action , RELEASEPATH ) )
 				action.exit( "getReleaseVerByLabel: unable to find prod distributive. Exiting" );
-
-			if( RELEASEDIR.indexOf( ' ' ) > 0 )
-				action.exit( "getReleaseVerByLabel: ambiguus distributives - " + RELEASEDIR );
 		}
 		else
-			RELEASEDIR = RELEASELABEL;
+			RELEASEPATH = "releases/" + RELEASELABEL;
 		
-		if( !RELEASELABEL.equals( RELEASEDIR ))
-			action.debug( "found release directory=" + RELEASEDIR + " by label=" + RELEASELABEL );
+		if( !RELEASELABEL.equals( RELEASEPATH ))
+			action.debug( "found release directory=" + RELEASEPATH + " by label=" + RELEASELABEL );
 		
-		return( RELEASEDIR );
+		return( RELEASEPATH );
 	}
 }
