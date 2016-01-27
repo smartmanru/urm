1571b51a5c658a69fd8ec3afac97b47152bee0c7
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-01-27 17:29:19 +0000

    add verifydeploy

--- src/ru/egov/urm/run/deploy/DeployCommand.java
+++ src/ru/egov/urm/run/deploy/DeployCommand.java
@@ -186,6 +186,7 @@ public class DeployCommand {
 	}
 
 	public void verifyDeploy( ActionBase action , ActionScope scope , DistStorage dist ) throws Exception {
+		dist.open( action );
 		ActionVerifyDeploy ca = new ActionVerifyDeploy( action , null , dist );
 		ca.runAll( scope );
 	}
