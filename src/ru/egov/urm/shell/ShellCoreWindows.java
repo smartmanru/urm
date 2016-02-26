3d83eda95f9996de92182cfa61af9e5bb7dcabfe
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-02-26 11:39:45 +0000

    add ccodepage

--- src/ru/egov/urm/shell/ShellCoreWindows.java
+++ src/ru/egov/urm/shell/ShellCoreWindows.java
@@ -52,8 +52,7 @@ public class ShellCoreWindows extends ShellCore {
 			execLine += " -i " + keyFile;
 
 		String cmdWin = Common.replace( cmd , "\\" , "\\\\" );
-		cmdWin = new String( cmdWin.getBytes() , "windows-1251" );
-		execLine += " " + executor.account.HOSTLOGIN + " " + Common.getQuoted( "cmd /c " + cmdWin );
+		execLine += " " + executor.account.HOSTLOGIN + " " + Common.getQuoted( "cmd /c 65001 & cmd /c " + cmdWin );
 		action.trace( executor.name + " execute: " + cmd );
 		return( execLine );
 	}
