04476b423b98b4ebdcdfa82c8f9fea842bed9ba0
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-04-25 14:59:40 +0000

    add install

---
+++ install/bin/deploy.cmd
@@ -0,0 +1,28 @@
+@echo off
+
+cd /d %~dp0
+
+set RELEASE=%1
+set ADDOPT=%2
+
+if "%RELEASE%" == "" (
+	echo RELEASE not set. Exiting
+	exit 1
+)
+echo deploy %RELEASE% ...
+
+set PATH=%cd%;%PATH%
+cd ..\..
+set ROOTPATH=%cd%
+call _context.cmd
+
+set RELEASEPATH=%ROOTPATH%\releases\%RELEASE%
+set PATH=%ROOTPATH%\install\jre7\bin;%PATH%
+
+set STDOPTS=%ADDOPT% -local -etcpath %RELEASEPATH%\urm.conf -distpath %RELEASEPATH%\urm.distr -hiddenpath %ROOTPATH%\secured
+
+cd %ROOTPATH%\install\master\bin
+call urm.cmd deploy redist %STDOPTS% default paymain
+
+cd %ROOTPATH%\install\master\bin
+call urm.cmd deploy getredistinfo %STDOPTS% -release default paymain
