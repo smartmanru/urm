9b31976001ba769bdfa1bcf7c937e75396a229f2
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-05-05 15:53:53 +0000

    fix

--- master/database/sql/postgres/linux/readcellvalue.sh
+++ master/database/sql/postgres/linux/readcellvalue.sh
@@ -10,7 +10,7 @@ P_COLUMN=$2
 P_CONDITION=$3
 
 export PGPASSWORD="$URMDB_PWD"
-VALUE=`echo "select 'value=$P_COLUMN as x from $P_TABLE where $P_CONDITION;" | psql -A -q -t -d $URMDB_DBNAME -h $URMDB_DBHOST -U $URMDB_USER`
+VALUE=`echo "select 'value=' || $P_COLUMN as x from $P_TABLE where $P_CONDITION;" | psql -A -q -t -d $URMDB_DBNAME -h $URMDB_DBHOST -U $URMDB_USER`
 
 if [[ "$VALUE" =~ "ERROR:" ]]; then
 	echo unexpected error: $VALUE
