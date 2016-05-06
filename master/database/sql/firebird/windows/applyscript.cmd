ba3067e28e6983f4f9356e3982621f6bec6e84ea
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-05-06 13:34:07 +0000

    fix

---
+++ master/database/sql/firebird/windows/applyscript.cmd
@@ -0,0 +1,9 @@
+set P_SQLFILE=%1
+set P_LOGFILE=%2
+
+%FBPATH%\isql -q -u %URMDB_USER% -p %URMDB_PWD% -i %P_SQLFILE% -o %P_LOGFILE% "%URMDB_DBHOST%:%URMDB_DBNAME%"
+SET status=%errorlevel%
+echo status=%status%
+
+if %status% == 0 exit /B 0
+exit /B 1
