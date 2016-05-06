448465d8981d1f98b47b6c714ba8f9532b69f6c4
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-05-06 12:44:11 +0000

    add fb

---
+++ master/database/init/firebird/windows/001-createadm.sql
@@ -0,0 +1,30 @@
+if (exists(select * from rdb$relations where RDB$RELATION_NAME = 'ADM_SCRIPTS')) then
+  execute statement 'drop table ADM_SCRIPTS';
+if (exists(select * from rdb$relations where RDB$RELATION_NAME = 'ADM_RELEASES')) then
+  execute statement 'drop table ADM_RELEASES';
+
+  CREATE TABLE adm_releases
+   (	
+	release varchar(30),
+	rel_p1 int default 0,
+	rel_p2 int default 0,
+	rel_p3 int default 0,
+	rel_p4 int default 0,
+	begin_apply_time timestamp without time zone,
+	end_apply_time timestamp without time zone,
+	rel_status char(1) ,
+	constraint pk_adm_releases primary key (release) using index idx_pk_adm_releases
+   ) ;
+
+  CREATE TABLE adm_scripts
+   (	
+	release varchar(30) references adm_releases,
+	delivery varchar(30),
+	key varchar(100),
+	schema varchar(30),
+	filename varchar(255),
+	begin_apply_time timestamp without time zone,
+	end_apply_time timestamp without time zone,
+	script_status char(1) ,
+	constraint pk_adm_scripts primary key (release, delivery, schema, key) using index idx_pk_adm_scripts
+   );
