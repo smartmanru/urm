b7ff453e002aff87b3b8e607f4ff9a22ad584896
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-05-06 15:21:58 +0000

    fix

--- master/database/init/firebird/windows/001-createadm.sql
+++ master/database/init/firebird/windows/001-createadm.sql
@@ -13,26 +13,26 @@ SET TERM ;#
 
   CREATE TABLE adm_releases
    (	
-	releasename varchar(30),
-	rel_p1 int default 0,
-	rel_p2 int default 0,
-	rel_p3 int default 0,
-	rel_p4 int default 0,
-	begin_apply_time timestamp without time zone,
-	end_apply_time timestamp without time zone,
-	rel_status char(1) ,
-	constraint pk_adm_releases primary key (releasename) using index idx_pk_adm_releases
+	zrelease varchar(30) ,
+	zrel_p1 int default 0 ,
+	zrel_p2 int default 0 ,
+	zrel_p3 int default 0 ,
+	zrel_p4 int default 0 ,
+	zbegin_apply_time timestamp ,
+	zend_apply_time timestamp ,
+	zrel_status char(1) ,
+	constraint pk_adm_releases primary key (zrelease) using index idx_pk_adm_releases
    ) ;
 
   CREATE TABLE adm_scripts
    (	
-	releasename varchar(30) references adm_releases,
-	delivery varchar(30),
-	key varchar(100),
-	schema varchar(30),
-	filename varchar(255),
-	begin_apply_time timestamp without time zone,
-	end_apply_time timestamp without time zone,
-	script_status char(1) ,
-	constraint pk_adm_scripts primary key (releasename, delivery, schema, key) using index idx_pk_adm_scripts
+	zrelease varchar(30) references adm_releases ,
+	zdelivery varchar(30) ,
+	zkey varchar(100) ,
+	zschema varchar(30) ,
+	zfilename varchar(255) ,
+	zbegin_apply_time timestamp ,
+	zend_apply_time timestamp ,
+	zscript_status char(1) ,
+	constraint pk_adm_scripts primary key (zrelease, zdelivery, zschema, zkey) using index idx_pk_adm_scripts
    );
