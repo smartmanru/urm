cd49d0b448aefb3fb666f25284f5ce3812bd97ca
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-05-12 13:04:31 +0000

    add schema

--- install/bin/deploy.cmd
+++ install/bin/deploy.cmd
@@ -3,10 +3,15 @@
 cd /d %~dp0
 
 set RELEASE=%1
-set ADDOPT=%2
+set CMD=%2
+set ADDOPT=%3
 
 if "%RELEASE%" == "" (
 	echo RELEASE not set. Exiting
+	exit 1
+)
+if "%CMD%" == "" (
+	echo CMD not set. Exiting
 	exit 1
 )
 echo deploy %RELEASE% ...
@@ -21,11 +26,45 @@ set PATH=%ROOTPATH%\install\jre7\bin;%PATH%
 
 set STDOPTS=%ADDOPT% -local -etcpath %RELEASEPATH%\urm.conf -distpath %RELEASEPATH%\urm.distr -hiddenpath %ROOTPATH%\secured\%C_CONTEXT_SECUREDPATH%
 
+set F_DBAPPLY=yes
+set F_REDIST=yes
+set F_DEPLOY=yes
+if "%CMD%" <> "all" if "%CMD%" <> "dbapply" set F_DBAPPLY=no
+if "%CMD%" <> "all" if "%CMD%" <> "redist" set F_REDIST=no
+if "%CMD%" <> "all" if "%CMD%" <> "deployredist" set F_DEPLOY=no
+
+if "%F_DBAPPLY%" == "yes" (
+cd %ROOTPATH%\install\master\bin
+call urm.cmd database dbapply %STDOPTS% -db maindb default all
+if %errorlevel% <> 0 (
+	echo unable to apply database changes
+	exit /B 1
+)
+)
+
+if "%F_REDIST%" == "yes" (
 cd %ROOTPATH%\install\master\bin
 call urm.cmd deploy redist %STDOPTS% default paymain
+if %errorlevel% <> 0 (
+	echo unable to redist release
+	exit /B 1
+)
 
 cd %ROOTPATH%\install\master\bin
 call urm.cmd deploy getredistinfo %STDOPTS% -release default paymain
+if %errorlevel% <> 0 (
+	echo unable to get dist info
+	exit /B 1
+)
+)
 
+if "%F_DEPLOY%" == "yes" (
 cd %ROOTPATH%\install\master\bin
-call urm.cmd deploy rollout %STDOPTS% default paymain
+call urm.cmd deploy deployredist %STDOPTS% default paymain
+if %errorlevel% <> 0 (
+	echo unable to redist release
+	exit /B 1
+)
+)
+
+exit /B 0
