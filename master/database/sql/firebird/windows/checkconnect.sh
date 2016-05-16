e81ee7839820d130f439172203dcf7b102534e79
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-05-16 07:51:19 +0000

    add sh-win

--- master/database/sql/firebird/windows/checkconnect.sh
+++ master/database/sql/firebird/windows/checkconnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "show database;" | $FBPATH/isql -q -u %URMDB_USER% -p %URMDB_PWD% "%URMDB_DBHOST%:%URMDB_DBNAME%"
+echo "show database;" | $FBPATH/isql -q -u $URMDB_USER -p $URMDB_PWD "$URMDB_DBHOST:$URMDB_DBNAME"
 F_STATUS=?
 echo status=$F_STATUS
 
