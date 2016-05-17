4f84b74eb96cd89a3da3a4ff8bbb6e572259a2bb
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-05-17 14:31:03 +0000

    fix

--- master/database/sql/firebird/windows/queryscript.cmd
+++ master/database/sql/firebird/windows/queryscript.cmd
@@ -1,7 +1,7 @@
 set P_SQLFILE=%1
 set P_LOGFILE=%2
 
-%FBPATH%\isql -q -u %URMDB_USER% -p %URMDB_PWD% -ch %URMDB_DBCHARSET% -i %P_SQLFILE% "%URMDB_DBHOST%:%URMDB_DBNAME%" > %P_LOGFILE% 2>&1
+%FBPATH%\isql -q -u %URMDB_USER% -p %URMDB_PWD% -ch %URMDB_CHARSET% -i %P_SQLFILE% "%URMDB_DBHOST%:%URMDB_DBNAME%" > %P_LOGFILE% 2>&1
 SET status=%errorlevel%
 echo status=%status%
 
