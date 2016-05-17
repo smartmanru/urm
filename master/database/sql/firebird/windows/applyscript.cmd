d6e6c56e0127c21d4af093f21b51d00b4ce23be2
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-05-17 14:56:15 +0000

    fix

--- master/database/sql/firebird/windows/applyscript.cmd
+++ master/database/sql/firebird/windows/applyscript.cmd
@@ -1,7 +1,8 @@
 set P_SQLFILE=%1
 set P_LOGFILE=%2
 
-%FBPATH%\isql -q -u %URMDB_USER% -p %URMDB_PWD% -ch %URMDB_CHARSET% -e -i %P_SQLFILE% "%URMDB_DBHOST%:%URMDB_DBNAME%" > %P_LOGFILE% 2>&1
+set XURMDB_CHARSET=%URMDB_CHARSET:windows-=WIN%
+%FBPATH%\isql -q -u %URMDB_USER% -p %URMDB_PWD% -ch %XURMDB_CHARSET% -e -i %P_SQLFILE% "%URMDB_DBHOST%:%URMDB_DBNAME%" > %P_LOGFILE% 2>&1
 SET status=%errorlevel%
 echo status=%status%
 
