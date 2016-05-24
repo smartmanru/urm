70824302c47801316affd7547699545bcff1707a
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-05-24 10:07:31 +0000

    add getdeployinfo

--- src/ru/egov/urm/meta/MetaEnv.java
+++ src/ru/egov/urm/meta/MetaEnv.java
@@ -104,6 +104,7 @@ public class MetaEnv {
 		if( propFile.isEmpty() )
 			return;
 		
+		action.debug( "load hidden properties file " + propFile );
 		secretProperties.loadRawFromFile( action , propFile );
 		secretProperties.moveRawAsStrings( action );
 	}
