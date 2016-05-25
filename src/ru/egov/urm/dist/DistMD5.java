98c72c96c98aa77e3b4e6d23f01d9ba171bb2404
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-05-25 16:17:28 +0000

    rework

--- src/ru/egov/urm/dist/DistMD5.java
+++ src/ru/egov/urm/dist/DistMD5.java
@@ -1,6 +1,8 @@
 package ru.egov.urm.dist;
 
+import ru.egov.urm.Common;
 import ru.egov.urm.action.ActionBase;
+import ru.egov.urm.meta.MetaDistrBinaryItem;
 import ru.egov.urm.meta.MetaReleaseTarget;
 import ru.egov.urm.meta.MetaReleaseTargetItem;
 
@@ -9,15 +11,20 @@ public class DistMD5 {
 	public String distItem;
 	public String distFile;
 	public String md5value;
+
+	public static String getDistItemRecord( ActionBase action , Dist dist , MetaDistrBinaryItem item ) throws Exception {
+		String deliveryFile = dist.getBinaryDistItemFile( action , item );
+		String md5value = dist.getDistItemMD5( action , item , deliveryFile );
+		String releaseFile = Common.getPath( item.delivery.FOLDER , deliveryFile );
+		return( item.KEY + ":" + releaseFile + ":" + md5value );
+	}
 	
 	public static String getManualItemRecord( ActionBase action , Dist dist , MetaReleaseTarget manualItem ) throws Exception {
-		String md5value = dist.getDistItemMD5( action , manualItem.distManualItem , manualItem.DISTFILE );
-		return( manualItem.distManualItem.KEY + ":" + manualItem.DISTFILE + ":" + md5value );
+		return( getDistItemRecord( action , dist , manualItem.distManualItem ) );
 	}
 
 	public static String getProjectItemRecord( ActionBase action , Dist dist , MetaReleaseTargetItem projectItem ) throws Exception {
-		String md5value = dist.getDistItemMD5( action , projectItem.distItem , projectItem.DISTFILE );
-		return( projectItem.distItem.KEY + ":" + projectItem.DISTFILE + ":" + md5value );
+		return( getDistItemRecord( action , dist , projectItem.distItem ) );
 	}
 	
 }
