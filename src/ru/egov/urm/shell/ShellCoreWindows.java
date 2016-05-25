a0a6338db6755261e961631c1d1eee4228e8ead7
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-05-25 11:25:47 +0000

    fix

--- src/ru/egov/urm/shell/ShellCoreWindows.java
+++ src/ru/egov/urm/shell/ShellCoreWindows.java
@@ -687,7 +687,8 @@ public class ShellCoreWindows extends ShellCore {
 		String cmdDir = getDirCmdIfDir( action , dir , 
 				"for /f %x in ('dir /O N /b /a-d ^| " + cmd + "') do certutil -hashfile %x MD5 | findstr /V " + 
 				Common.getQuoted( "MD5 CertUtil" ) + " > " + tmpFile );
-		runCommand( action , cmdDir , CommandOutput.LOGLEVEL_TRACE );
+		executor.customCheckErrorsDebug( action , cmdDir );
+		
 		String[] lines = executor.getFileLines( action , tmpFile );
 		for( int k = 0; k < lines.length; k++ )
 			lines[ k ] = Common.replace( lines[ k ] , " " , "" );
