7e57658b504cb1b124478ad4f645fd825f5afd90
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-06-01 14:13:25 +0000

    rework cumulative

---
+++ src/ru/egov/urm/action/database/ActionManageRegistry.java
@@ -0,0 +1,85 @@
+package ru.egov.urm.action.database;
+
+import java.util.Map;
+
+import ru.egov.urm.Common;
+import ru.egov.urm.action.ActionBase;
+import ru.egov.urm.action.ActionScopeTarget;
+import ru.egov.urm.meta.MetaDistrDelivery;
+import ru.egov.urm.meta.MetaEnvServer;
+
+public class ActionManageRegistry extends ActionBase {
+
+	String RELEASEVER;
+	String CMD;
+	MetaDistrDelivery optDelivery;
+	String indexScope;
+	
+	public ActionManageRegistry( ActionBase action , String stream , String RELEASEVER , String CMD , MetaDistrDelivery optDelivery , String indexScope ) {
+		super( action , stream );
+		this.RELEASEVER = RELEASEVER;
+		this.CMD = CMD;
+		this.optDelivery = optDelivery;
+		this.indexScope = indexScope;
+	}
+
+	@Override protected boolean executeScopeTarget( ActionScopeTarget target ) throws Exception {
+		MetaEnvServer server = target.envServer;
+		DatabaseClient client = new DatabaseClient();
+		if( !client.checkConnect( this , server ) )
+			exit( "unable to connect to server=" + server.NAME );
+
+		info( RELEASEVER + ": " + CMD + " database registry ..." );
+		DatabaseRegistry registry = DatabaseRegistry.getRegistry( this , client );
+		registry.setActiveRelease( this , RELEASEVER );
+		
+		if( CMD.equals( "print" ) )
+			executePrintRegistry( registry );
+		else
+		if( CMD.equals( "correct" ) )
+			executeCorrectRegistry( registry );
+		else
+		if( CMD.equals( "rollback" ) )
+			executeRollbackRegistry( registry );
+		else
+		if( CMD.equals( "drop" ) )
+			executeDropRegistry( registry );
+		else
+			exit( "unexpected manage command=" + CMD );
+		
+		return( true );
+	}
+
+	private void executePrintRegistry( DatabaseRegistry registry ) throws Exception {
+		if( registry.isReleaseUnknown( this ) )
+			exit( "unknown release version=" + RELEASEVER );
+		
+		comment( "DATABASE RELEASE STATUS" );
+		comment( "\tversion=" + RELEASEVER );
+		String state = ( registry.isReleaseFinished( this ) )? "APPLIED" : "INCOMPLETE";
+		comment( "\tstate=" + state );
+		
+		registry.readIncompleteScripts( this );
+		for( String delivery : registry.getStateDeliveries( this ) ) {
+			comment( "DELIVERY: " + delivery );
+			Map<String,String> data = registry.getStateData( this , delivery );
+			DatabaseScriptFile file = new DatabaseScriptFile();
+			for( String name : Common.getSortedKeys( data ) ) {
+				file.setDistFile( this , name );
+				String status = data.get( name );
+				String value = ( status.equals( DatabaseRegistry.SCRIPT_STATUS_APPLIED ) )? "OK" : "FAILED";
+				comment( "\tkey=" + file.getDistKey() + ", status=" + value );
+			}
+		}
+	}
+	
+	private void executeCorrectRegistry( DatabaseRegistry registry ) throws Exception {
+	}
+	
+	private void executeRollbackRegistry( DatabaseRegistry registry ) throws Exception {
+	}
+	
+	private void executeDropRegistry( DatabaseRegistry registry ) throws Exception {
+	}
+	
+}
