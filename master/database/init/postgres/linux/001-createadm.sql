c541b32ae29906aa6f17e749d6203af8be7e95e0
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-06-02 09:10:03 +0000

    fix

--- master/database/init/postgres/linux/001-createadm.sql
+++ master/database/init/postgres/linux/001-createadm.sql
@@ -3,31 +3,31 @@ drop table if exists adm_releases;
 
   CREATE TABLE adm_releases
    (	
-	release varchar(30),
-	rel_p1 integer default 0,
-	rel_p2 integer default 0,
-	rel_p3 integer default 0,
-	rel_p4 integer default 0,
-	begin_apply_time timestamp without time zone,
-	end_apply_time timestamp without time zone,
-	rel_status char(1)
+	zrelease varchar(30),
+	zrel_p1 integer default 0,
+	zrel_p2 integer default 0,
+	zrel_p3 integer default 0,
+	zrel_p4 integer default 0,
+	zbegin_apply_time timestamp without time zone,
+	zend_apply_time timestamp without time zone,
+	zrel_status char(1)
    ) ;
 
-  CREATE UNIQUE INDEX CONCURRENTLY adm_releases_pk ON adm_releases (release);
+  CREATE UNIQUE INDEX CONCURRENTLY adm_releases_pk ON adm_releases (zrelease);
   ALTER TABLE adm_releases ADD CONSTRAINT adm_releases_pk PRIMARY KEY USING INDEX adm_releases_pk;
 
   CREATE TABLE adm_scripts
    (	
-	release varchar(30),
-	delivery varchar(30),
-	key varchar(100),
-	schema varchar(30),
-	filename varchar(255),
-	begin_apply_time timestamp without time zone,
-	end_apply_time timestamp without time zone,
-	script_status char(1)
+	zrelease varchar(30),
+	zdelivery varchar(30),
+	zkey varchar(100),
+	zschema varchar(30),
+	zfilename varchar(255),
+	zbegin_apply_time timestamp without time zone,
+	zend_apply_time timestamp without time zone,
+	zscript_status char(1)
    );
 
-  CREATE UNIQUE INDEX CONCURRENTLY adm_scripts_pk ON adm_scripts (release, delivery, schema, key);
+  CREATE UNIQUE INDEX CONCURRENTLY adm_scripts_pk ON adm_scripts (zrelease, zdelivery, zschema, zkey);
   ALTER TABLE adm_scripts ADD CONSTRAINT adm_scripts_pk PRIMARY KEY USING INDEX adm_scripts_pk;
-  ALTER TABLE adm_scripts ADD CONSTRAINT adm_scripts_fk FOREIGN KEY (release) REFERENCES adm_releases (release) MATCH FULL;
+  ALTER TABLE adm_scripts ADD CONSTRAINT adm_scripts_fk FOREIGN KEY (zrelease) REFERENCES adm_releases (zrelease) MATCH FULL;
