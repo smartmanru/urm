e7342b25d9cb3abb5d678f052d1eadc55a334b56
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-06-06 14:59:33 +0000

    rework

--- master/bin/urm.cmd
+++ master/bin/urm.cmd
@@ -2,4 +2,4 @@
 @cd ..\..
 
 @set PRODUCT_HOME=%CD%
-@java -cp master/bin/urm.jar;master/lib/jna-4.1.0.jar -Duser.language=ru -Durm.os=windows -Dproduct.home=%PRODUCT_HOME% -Dbuild.mode=%C_CONTEXT_VERSIONMODE% -Denv=%C_CONTEXT_ENV% -Ddc=%C_CONTEXT_DC% org.urm.Engine %*
+@java -cp master/bin/urm.jar;master/lib/jna-4.1.0.jar -Duser.language=ru -Durm.os=windows -Dproduct.home=%PRODUCT_HOME% -Dbuild.mode=%C_CONTEXT_VERSIONMODE% -Denv=%C_CONTEXT_ENV% -Ddc=%C_CONTEXT_DC% org.urm.client.Engine %*
