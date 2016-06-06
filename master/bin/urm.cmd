c3d573101a69021e07f29148bd5dc58ea4e29e2c
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-06-06 15:14:56 +0000

    rework

--- master/bin/urm.cmd
+++ master/bin/urm.cmd
@@ -1,5 +1,15 @@
-@cd %~dp0
-@cd ..\..
+@echo off
 
-@set PRODUCT_HOME=%CD%
-@java -cp master/bin/urm.jar;master/lib/jna-4.1.0.jar -Duser.language=ru -Durm.os=windows -Dproduct.home=%PRODUCT_HOME% -Dbuild.mode=%C_CONTEXT_VERSIONMODE% -Denv=%C_CONTEXT_ENV% -Ddc=%C_CONTEXT_DC% org.urm.client.Engine %*
+cd %~dp0
+cd ..\..
+
+set PRODUCT_HOME=%CD%
+
+if "%C_CONTEXT_URMSERVER%" == "" (
+	set JAVACP=master/bin/urms.jar;master/lib/jna-4.1.0.jar
+)
+else (
+	set JAVACP=master/bin/urmc.jar
+)
+
+java -cp %JAVACP% -Duser.language=ru -Durm.os=windows -Dproduct.home=%PRODUCT_HOME% -Dbuild.mode=%C_CONTEXT_VERSIONMODE% -Denv=%C_CONTEXT_ENV% -Ddc=%C_CONTEXT_DC% org.urm.client.Engine %*
--- master/bin/urm.sh
+++ master/bin/urm.sh
@@ -1,6 +1,14 @@
 #!/bin/bash 
+
 cd `dirname $0`
 cd ../..
 
 PRODUCT_HOME=`pwd`
-java -cp master/bin/urm.jar:master/lib/jna-4.1.0.jar -Duser.language=ru -Dfile.encoding=utf-8 -Durm.os=linux -Dproduct.home=$PRODUCT_HOME -Dbuild.mode=$C_CONTEXT_VERSIONMODE -Denv=$C_CONTEXT_ENV -Ddc=$C_CONTEXT_DC org.urm.client.Engine "$@"
+
+if [ "$C_CONTEXT_URMSERVER" = "" ]; then
+	JAVACP=master/bin/urms.jar:master/lib/jna-4.1.0.jar
+else
+	JAVACP=master/bin/urmc.jar
+fi
+
+java -cp $JAVACP -Duser.language=ru -Dfile.encoding=utf-8 -Durm.os=linux -Dproduct.home=$PRODUCT_HOME -Dbuild.mode=$C_CONTEXT_VERSIONMODE -Denv=$C_CONTEXT_ENV -Ddc=$C_CONTEXT_DC org.urm.client.Engine "$@"
