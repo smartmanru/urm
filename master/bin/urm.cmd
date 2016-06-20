3fcd12e5722c04e8999ad1e60866b1646a47a648
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-06-20 09:39:48 +0000

    fix

--- master/bin/urm.cmd
+++ master/bin/urm.cmd
@@ -21,6 +21,7 @@ if "%C_CONTEXT_URMSERVER%" == "" (
 set x=0
 if "%1" == "-trace" set x=1
 if "%2" == "-trace" set x=1
+if "%3" == "-trace" set x=1
 if "%x" == "1" (
 	echo run: java -cp %JAVACP% -Duser.language=ru -Durm.mode=%C_URM_MODE% -Durm.os=windows -Durm.installpath=%INSTALL_PATH% -Durm.server=%C_CONTEXT_URMSERVER% -Durm.product=%C_CONTEXT_PRODUCT% -Durm.build=%C_CONTEXT_VERSIONMODE% -Durm.env=%C_CONTEXT_ENV% -Durm.dc=%C_CONTEXT_DC% %C_UMR_CLASS% %*
 )
