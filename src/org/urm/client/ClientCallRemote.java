964197a7ea539a2c263b128a121efa3cb0620837
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-06-21 14:00:24 +0000

    rework

--- src/org/urm/client/ClientCallRemote.java
+++ src/org/urm/client/ClientCallRemote.java
@@ -1,5 +1,12 @@
 package org.urm.client;
 
+import java.util.Arrays;
+
+import javax.management.MBeanServerConnection;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
 import org.urm.common.ExitException;
 import org.urm.common.action.CommandBuilder;
 import org.urm.common.action.CommandMeta;
@@ -7,6 +14,17 @@ import org.urm.common.action.CommandMeta;
 public class ClientCallRemote {
 
 	public boolean runClient( CommandBuilder builder , CommandMeta commandInfo ) throws Exception {
+		JMXServiceURL url = new JMXServiceURL( "service:jmx:rmi:///jndi/rmi://" + builder.rc.serverHostPort + "/jmxrmi" );
+		JMXConnector jmxc = JMXConnectorFactory.connect( url , null );
+		MBeanServerConnection mbsc = jmxc.getMBeanServerConnection();
+		
+		System.out.println("\nDomains:");
+		String domains[] = mbsc.getDomains();
+		Arrays.sort(domains);
+		for (String domain : domains) {
+			System.out.println("\tDomain = " + domain);
+		}
+		
 		throw new ExitException( "sorry, not implemented yet" );
 	}
 	
