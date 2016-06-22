37b7a902989b21545716a6fdcf95293898a2ac14
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-06-22 14:20:13 +0000

    rework

---
+++ src/org/urm/common/jmx/ClientCallRemote.java
@@ -0,0 +1,57 @@
+package org.urm.common.jmx;
+
+import javax.management.MBeanServerConnection;
+import javax.management.Notification;
+import javax.management.NotificationListener;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+
+import org.urm.common.action.CommandBuilder;
+import org.urm.common.action.CommandMeta;
+
+public class ClientCallRemote implements NotificationListener {
+
+	public boolean runClient( CommandBuilder builder , CommandMeta commandInfo ) throws Exception {
+		String URL = "service:jmx:rmi:///jndi/rmi://" + builder.execrc.serverHostPort + "/jmxrmi";
+		JMXServiceURL url = new JMXServiceURL( URL );
+		
+		JMXConnector jmxc = null;
+		MBeanServerConnection mbsc = null;
+		
+		try {
+			jmxc = JMXConnectorFactory.connect( url , null );
+			mbsc = jmxc.getMBeanServerConnection();
+		}
+		catch( Throwable e ) {
+			System.out.println( "unable to connect to: " + URL );
+			e.printStackTrace();
+			return( false );
+		}
+		
+		String name = builder.getCommandMBeanName( builder.execrc.productDir , commandInfo.name );
+		try {
+			ObjectName mbeanName = new ObjectName( name );
+			mbsc.addNotificationListener( mbeanName , this , null , null );
+			mbsc.invoke( mbeanName , builder.options.action , null , null );
+		}
+		catch( Throwable e ) {
+			System.out.println( "unable to call operation: " + name );
+			e.printStackTrace();
+			return( false );
+		}
+		
+		synchronized( this ) {
+			wait();
+		}
+		
+		return( true );
+	}
+
+	public void handleNotification( Notification notif , Object handback ) {
+		ActionLogNotification n = ( ActionLogNotification )notif;
+		System.out.println( n.getMessage() );
+	}
+	
+}
