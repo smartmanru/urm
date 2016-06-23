df2ed3205c3704117523432438a23235ce6c733b
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-06-23 08:57:45 +0000

    fix

--- master/bin/urm.sh
+++ master/bin/urm.sh
@@ -12,13 +12,13 @@ else
 	C_UMR_CLASS=org.urm.client.Main
 fi
 
-if [ "$C_URM_SERVER" = "" ]; then
+if [ "$1" = "-local" ] || [ "$2" = "-local" ] || [ "$3" = "-local" ] || [ "$4" = "-local" ] || [ "$C_URM_SERVER" = "" ]; then
 	JAVACP=master/bin/urms.jar:master/lib/jmxtools-1.2.1.jar:master/lib/jna-4.1.0.jar
 else
 	JAVACP=master/bin/urmc.jar:master/lib/jmxtools-1.2.1.jar
 fi
 
-if [ "$1" = "-trace" ] || [ "$2" = "-trace" ] || [ "$3" = "-trace" ]; then
+if [ "$1" = "-trace" ] || [ "$2" = "-trace" ] || [ "$3" = "-trace" ] || [ "$4" = "-trace" ]; then
 	echo run: java -cp $JAVACP -Duser.language=ru -Dfile.encoding=utf-8 -Durm.mode=$C_URM_MODE -Durm.os=linux -Durm.installpath=$C_URM_INSTALLPATH -Durm.server=$C_URM_SERVER -Durm.product=$C_URM_PRODUCT -Durm.build=$C_URM_VERSIONMODE -Durm.env=$C_URM_ENV -Durm.dc=$C_URM_DC $C_UMR_CLASS "$@"
 fi
 
