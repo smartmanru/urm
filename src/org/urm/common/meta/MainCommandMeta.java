d9b40ddc038509f410f992639fb20a6fe8feb431
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-06-23 11:30:18 +0000

    fix

--- src/org/urm/common/meta/MainCommandMeta.java
+++ src/org/urm/common/meta/MainCommandMeta.java
@@ -21,7 +21,6 @@ public class MainCommandMeta extends CommandMeta {
 		String cmdOpts = "OPT_HOST,OPT_PORT";
 		super.defineAction( CommandMethod.newCritical( "configure" , true , "configure proxy files" , cmdOpts , "configure [OPTIONS] {default|server|standalone} [envname [dcname]]" ) );
 		super.defineAction( CommandMethod.newCritical( "svnsave" , true , "save master file set in svn" , cmdOpts , "svnsave [OPTIONS]" ) );
-		cmdOpts = "OPT_PORT";
 		super.defineAction( CommandMethod.newNormal( "server" , true , "server control" , cmdOpts , "srver [OPTIONS] {start|stop|status}" ) );
 	}
 	
