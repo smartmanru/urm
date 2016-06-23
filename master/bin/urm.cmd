272e641bfcba4fc1117c8c4ab24631f35e9233f3
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-06-23 09:04:08 +0000

    fix

--- master/bin/urm.cmd
+++ master/bin/urm.cmd
@@ -18,7 +18,7 @@ if "%2" == "-local" set x=1
 if "%3" == "-local" set x=1
 if "%4" == "-local" set x=1
 if "%C_URM_SERVER%" == "" set x=1
-if "%x" == "1" (
+if "%x%" == "1" (
 	set JAVACP=master/bin/urms.jar;master/lib/jmxtools-1.2.1.jar;master/lib/jna-4.1.0.jar
 ) else (
 	set JAVACP=master/bin/urmc.jar;master/lib/jmxtools-1.2.1.jar
@@ -29,7 +29,7 @@ if "%1" == "-trace" set x=1
 if "%2" == "-trace" set x=1
 if "%3" == "-trace" set x=1
 if "%4" == "-trace" set x=1
-if "%x" == "1" (
+if "%x%" == "1" (
 	echo run: java -cp %JAVACP% -Duser.language=ru -Durm.mode=%C_URM_MODE% -Durm.os=windows -Durm.installpath=%C_URM_INSTALLPATH% -Durm.server=%C_URM_SERVER% -Durm.product=%C_URM_PRODUCT% -Durm.build=%C_URM_VERSIONMODE% -Durm.env=%C_URM_ENV% -Durm.dc=%C_URM_DC% %C_UMR_CLASS% %*
 )
 
