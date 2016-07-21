dd34671e2013f9d6c9057373938bf97f5c28369e
vladimir.savchik <vladimir.savchik@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-07-21 12:53:19 +0000

    rework

---
+++ src/org/urm/server/action/main/ActionWebSession.java
@@ -0,0 +1,11 @@
+package org.urm.server.action.main;
+
+import org.urm.server.action.ActionBase;
+
+public class ActionWebSession extends ActionBase {
+
+	public ActionWebSession( ActionBase action , String stream ) {
+		super( action , stream );
+	}
+
+}
