cf097bdc6bc64e4ccbe2b252209ef1793808d8e3
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-09-14 09:24:31 +0000

    fix

---
+++ src/org/urm/action/ActionSetItem.java
@@ -0,0 +1,75 @@
+package org.urm.action;
+
+
+public class ActionSetItem implements Runnable {
+
+	ActionSet set;
+    boolean threadFailed;
+    Exception exceptionCatched;
+    private boolean failed;
+	
+	boolean runSimple = false;
+	boolean runScope = false;
+	
+	ActionBase action;
+	ActionScope scope;
+	
+	public ActionSetItem( ActionSet set ) {
+		this.set = set;
+	}
+
+	public void createSimple( ActionBase action ) throws Exception {
+		runSimple = true;
+		this.action = action;
+	}
+
+	public void createScope( ActionBase action , ActionScope scope ) throws Exception {
+		runScope = true;
+		this.action = action;
+		this.scope = scope;
+	}
+
+    public void run() {
+    	failed = false;
+    	
+        try {
+            threadFailed = false;
+            execute();
+
+            if( action.isFailed() ) {
+            	failed = true;
+            	action.trace( action.NAME + ": failed=true" );
+            }
+            
+            synchronized( set.threadGroup ) {
+                set.threadGroup.notifyAll();
+            }
+        }
+        catch (Exception e) {
+        	failed = true;
+            threadFailed = true;
+
+            // output error message
+            exceptionCatched = e;
+            action.log( exceptionCatched );
+        }
+    }
+
+    private void execute() throws Exception {
+    	if( runSimple ) {
+    		if( !action.runSimple() )
+    			failed = true;
+    	}
+    	else
+    	if( runScope ) {
+    		if( !action.runAll( scope ) )
+    			failed = true;
+    	}
+    	else
+    		action.exitUnexpectedState();
+    }
+    
+    public boolean isFailed() {
+    	return( failed );
+    }
+}
