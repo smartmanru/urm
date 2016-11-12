f3fe8659a92d826b32063ddfdde011e3b6820274
builder <builder@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-11-12 13:37:37 +0000

    test 29

---
+++ src/org/urm/engine/storage/SourcePath.java
@@ -0,0 +1,14 @@
+package org.urm.engine.storage;
+
+public class SourcePath {
+
+	// local: urm.unstallpath/localServerRootBasedPath
+	// vcs: resourcepath/vcsDataFolder/vcsDataFolderBasedPath 
+	public String localServerRootBasedPath;
+	public String vcsDataFolder;
+	public String vcsDataFolderBasedPath; 
+	
+	public SourcePath() {
+	}
+	
+}
