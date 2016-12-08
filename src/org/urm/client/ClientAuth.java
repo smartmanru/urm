d36dbb6eecf0fffea5cff30b0d94aba1e7fef273
builder <builder@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2016-12-08 16:55:15 +0000

    prod

---
+++ src/org/urm/client/ClientAuth.java
@@ -0,0 +1,108 @@
+package org.urm.client;
+
+import java.io.Console;
+import java.io.File;
+
+import org.urm.common.Common;
+import org.urm.common.action.CommandBuilder;
+import org.urm.common.action.CommandOptions;
+
+public class ClientAuth {
+
+	ClientEngine client;
+	
+	public static String urmUserVar = "urm.user";
+	public static String urmPasswordVar = "urm.password";
+	public static String urmKeyVar = "urm.key";
+
+	public static String urmAuthFile = "auth.txt";
+	
+	public ClientAuth( ClientEngine client ) {
+		this.client = client;
+	}
+	
+	public boolean setAuth( CommandBuilder builder , CommandOptions options ) throws Exception {
+		String value = runClientAuthGetFile( builder , options );
+		if( value == null )
+			return( false );
+
+		String authDir = builder.execrc.authPath;
+		if( authDir.isEmpty() )
+			authDir = Common.getPath( builder.execrc.installPath , ".auth" );
+		String authFile = Common.getPath( authDir , urmAuthFile );
+				
+		authDir = builder.execrc.getLocalPath( authDir );
+		authFile = builder.execrc.getLocalPath( authFile );
+		
+		File folder = new File( authDir );
+		if( !folder.exists() ) {
+			if( !folder.mkdirs() ) {
+				builder.out( "Unable to create auth folder" );
+				return( false );
+			}
+			
+			folder.setReadable( true , true );
+			folder.setWritable( true , true );
+			folder.setExecutable( true , true );
+		}
+		
+		Common.createFileFromString( authFile , "" );
+		File file = new File( authFile );
+		file.setReadable( true , true );
+		file.setWritable( true , true );
+		
+		Common.createFileFromString( authFile , value );
+		return( true );
+	}
+	
+	private String runClientAuthGetFile( CommandBuilder builder , CommandOptions options ) throws Exception {
+		String s = "";
+		
+		String user = options.getParamValue( "OPT_USER" );
+		String key = options.getParamValue( "OPT_KEY" );
+		String password = options.getParamValue( "OPT_PASSWORD" );
+		if( user.isEmpty() ) {
+			builder.out( "User parameter is required" );
+			return( null );
+		}
+		
+		if( key.isEmpty() && password.isEmpty() ) {
+			Console console = System.console();
+			while( true ) {
+				console.printf( "Please enter your password: ");
+				char[] passwordChars = console.readPassword();
+				String passwordString = new String( passwordChars );
+				if( !password.isEmpty() ) {
+					s = setEnvVariable( s , urmUserVar , user );
+					s = setEnvVariable( s , urmPasswordVar , passwordString );
+					return( s );
+				}
+			}
+		}
+		
+		if( key.isEmpty() == false && password.isEmpty() == false ) {
+			builder.out( "Cannot set both key and password parameters" );
+			return( null );
+		}
+		
+		if( key.isEmpty() && password.isEmpty() == false ) {
+			s = setEnvVariable( s , urmUserVar , user );
+			s = setEnvVariable( s , urmPasswordVar , password );
+			return( s );
+		}
+		
+		s = setEnvVariable( s , urmUserVar , user );
+		s = setEnvVariable( s , urmKeyVar , key );
+		return( s );
+	}
+
+	public String setEnvVariable( String s , String var , String value ) {
+		s += var + "=" + value + "\n";
+		return( s );
+	}
+
+	public boolean getAuth( CommandBuilder builder , CommandOptions options ) throws Exception {
+		return( true );
+	}
+	
+}
