5865e1f2d0f182cdc97b7ee9c936dc7d0cdcce69
builder <builder@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2017-02-02 15:05:03 +0000

    1.0.5

--- src/org/urm/engine/ServerBlotterReleaseItem.java
+++ src/org/urm/engine/ServerBlotterReleaseItem.java
@@ -1,5 +1,6 @@
 package org.urm.engine;
 
+import org.urm.common.Common;
 import org.urm.engine.dist.DistRepositoryItem;
 
 public class ServerBlotterReleaseItem extends ServerBlotterItem {
@@ -7,6 +8,7 @@ public class ServerBlotterReleaseItem extends ServerBlotterItem {
 	public DistRepositoryItem repoItem;
 	
 	public String INFO_PRODUCT;
+	public String INFO_TYPE;
 	
 	public ServerBlotterReleaseItem( ServerBlotterSet blotterSet , String ID ) {
 		super( blotterSet , ID );
@@ -15,6 +17,18 @@ public class ServerBlotterReleaseItem extends ServerBlotterItem {
 	public void createReleaseItem( DistRepositoryItem repoItem ) {
 		this.repoItem = repoItem;
 		this.INFO_PRODUCT = repoItem.dist.meta.name;
+		
+		int count = Common.splitDotted( Common.getPartBeforeFirst( repoItem.dist.RELEASEDIR , "-" ) ).length;
+		String type = "";
+		if( count == 1 || count == 2 )
+			type = "major";
+		else
+		if( count == 3 )
+			type = "minor";
+		else
+		if( count == 4 )
+			type = "patch";
+		this.INFO_TYPE = type;
 	}
 	
 }
