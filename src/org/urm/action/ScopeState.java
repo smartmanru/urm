576d945e935abfe88f7b6a125291fe23ce602ea4
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2017-04-23 21:37:55 +0000

    info

--- src/org/urm/action/ScopeState.java
+++ src/org/urm/action/ScopeState.java
@@ -129,10 +129,13 @@ public class ScopeState {
 		this.state = state;
 		action.eventSource.finishScopeItem( this );
 		
-		ScopeState notifyParent = parent;
-		while( notifyParent != null ) {
-			notifyParent.action.eventSource.finishScopeItem( ServerEvents.EVENT_FINISHCHILDSTATE , this );
+		if( action instanceof ActionBase ) {
+			ActionBase notifyParent = ( ActionBase )action;
 			notifyParent = notifyParent.parent;
+			while( notifyParent != null ) {
+				notifyParent.eventSource.finishScopeItem( ServerEvents.EVENT_FINISHCHILDSTATE , this );
+				notifyParent = notifyParent.parent;
+			}
 		}
 	}
 
