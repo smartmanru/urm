c69486fba5cc3b97654f657f2b9823680130f702
builder <builder@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2017-11-29 21:30:05 +0000

    fix

---
+++ src/org/urm/db/engine/_Error.java
@@ -0,0 +1,15 @@
+package org.urm.db.engine;
+
+import org.urm.common.RunErrorClass;
+
+public class _Error {
+
+	public static final int ErrorBase = RunErrorClass.BaseDBEngine;
+	public static final int ErrorInternalBase = ErrorBase + RunErrorClass.InternalBase;
+	
+	public static final int GroupNotEmpty0 = ErrorBase + 1;
+	public static final int DatacenterNotEmpty0 = ErrorBase + 2;
+	public static final int NetworkNotEmpty0 = ErrorBase + 3;
+	public static final int HostNotEmpty0 = ErrorBase + 4;
+	
+}
