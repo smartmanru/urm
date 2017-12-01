773fed31234fd8ce2530cc819308ccfdfbd0b4b5
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2017-12-01 11:31:06 +0000

    fix

---
+++ src/org/urm/meta/engine/EngineLifecycles.java
@@ -0,0 +1,98 @@
+package org.urm.meta.engine;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.urm.common.Common;
+import org.urm.db.core.DBEnums.*;
+import org.urm.engine.blotter.EngineBlotterSet;
+import org.urm.engine.blotter.EngineBlotter.BlotterType;
+import org.urm.meta.EngineCore;
+import org.urm.meta.EngineObject;
+
+public class EngineLifecycles extends EngineObject {
+
+	public EngineCore core;
+	
+	private Map<String,ReleaseLifecycle> lcMap;
+	private Map<Integer,ReleaseLifecycle> lcMapById;
+	
+	public EngineLifecycles( EngineCore core ) {
+		super( null );
+		this.core = core;
+		lcMap = new HashMap<String,ReleaseLifecycle>(); 
+		lcMapById = new HashMap<Integer,ReleaseLifecycle>();
+	}
+	
+	@Override
+	public String getName() {
+		return( "server-lifecycles" );
+	}
+	
+	public void addLifecycle( ReleaseLifecycle lc ) {
+		lcMap.put( lc.NAME , lc );
+		lcMapById.put( lc.ID , lc );
+	}
+
+	public void updateLifecycle( ReleaseLifecycle lc ) throws Exception {
+		Common.changeMapKey( lcMap , lc , lc.NAME );
+	}
+	
+	public ReleaseLifecycle findLifecycle( String name ) {
+		return( lcMap.get( name ) );
+	}
+
+	public ReleaseLifecycle getLifecycle( String name ) throws Exception {
+		ReleaseLifecycle lc = findLifecycle( name );
+		if( lc == null )
+			Common.exit1( _Error.UnknownLifecycle1 , "unknown lifecycle name=" + name , name );
+		return( lc );
+	}
+
+	public ReleaseLifecycle getLifecycle( int id ) throws Exception {
+		ReleaseLifecycle lc = lcMapById.get( id );
+		if( lc == null )
+			Common.exit1( _Error.UnknownLifecycle1 , "unknown lifecycle id=" + id , "" + id );
+		return( lc );
+	}
+
+	public void addPhase( LifecyclePhase phase ) {
+		phase.lc.addPhase( phase );
+	}
+
+	
+	public String[] getLifecycleNames() {
+		return( Common.getSortedKeys( lcMap ) );
+	}
+
+	public ReleaseLifecycle[] getLifecycles() {
+		return( lcMap.values().toArray( new ReleaseLifecycle[0] ) );
+	}
+	
+	public String[] getLifecycles( DBEnumLifecycleType type , boolean enabledOnly ) {
+		List<String> list = new LinkedList<String>();
+		for( String lcName : Common.getSortedKeys( lcMap ) ) {
+			ReleaseLifecycle lc = lcMap.get( lcName );
+			if( lc.LIFECYCLE_TYPE == type ) {
+				if( enabledOnly == false || lc.ENABLED )
+					list.add( lcName );
+			}
+		}
+		return( list.toArray( new String[0] ) );
+	}
+
+	public void removeLifecycle( ReleaseLifecycle lc ) throws Exception {
+		lcMap.remove( lc.NAME );
+		lcMapById.remove( lc.ID );
+	}
+
+	public boolean isUsed( ReleaseLifecycle lc ) {
+		EngineBlotterSet blotter = core.engine.blotter.getBlotterSet( BlotterType.BLOTTER_RELEASE );
+		if( blotter.checkLifecycleUsed( lc.NAME ) )
+			return( true );
+		return( false );
+	}
+	
+}
---
+++ src/org/urm/meta/engine/LifecyclePhase.java
@@ -0,0 +1,90 @@
+package org.urm.meta.engine;
+
+import org.urm.db.core.DBEnums.*;
+import org.urm.meta.EngineObject;
+
+public class LifecyclePhase extends EngineObject {
+
+	public static String PROPERTY_NAME = "name";
+	public static String PROPERTY_DESC = "desc";
+	public static String PROPERTY_STAGE = "stage";
+	public static String PROPERTY_STAGE_POS = "stage_pos";
+	public static String PROPERTY_UNLIMITED = "unlimited";
+	public static String PROPERTY_START_DAY = "start_day";
+	public static String PROPERTY_DAYS = "days";
+	
+	ReleaseLifecycle lc;
+	
+	public int ID;
+	public String NAME;
+	public String DESC;
+	public DBEnumLifecycleStageType LIFECYCLESTAGE_TYPE;
+	public int STAGE_POS;
+	public boolean UNLIMITED;
+	public boolean START_DAY;
+	public int DAYS;
+	public int CV;
+	
+	public LifecyclePhase( ReleaseLifecycle lc ) {
+		super( lc );
+		this.lc = lc;
+	}
+	
+	@Override
+	public String getName() {
+		return( NAME );
+	}
+	
+	public LifecyclePhase copy( ReleaseLifecycle rlc ) throws Exception {
+		LifecyclePhase r = new LifecyclePhase( rlc );
+		r.ID = ID;
+		r.NAME = NAME;
+		r.DESC = DESC;
+		r.LIFECYCLESTAGE_TYPE = LIFECYCLESTAGE_TYPE;
+		r.STAGE_POS = STAGE_POS;
+		r.UNLIMITED = UNLIMITED;
+		r.START_DAY = START_DAY;
+		r.DAYS = DAYS;
+		r.CV = CV;
+		return( r );
+	}
+
+	public void createPhase( String name , String desc , DBEnumLifecycleStageType stage , int pos , boolean unlimited , boolean startDay , int days ) throws Exception {
+		modifyPhase( name , desc , stage , pos , unlimited , startDay , days );
+	}
+	
+	public void modifyPhase( String name , String desc , DBEnumLifecycleStageType stage , int pos , boolean unlimited , boolean startDay , int days ) throws Exception {
+		this.NAME = name;
+		this.DESC = desc;
+		this.LIFECYCLESTAGE_TYPE = stage;
+		this.STAGE_POS = pos;
+		this.UNLIMITED = unlimited;
+		this.START_DAY = startDay;
+		this.DAYS = days;
+	}
+	
+	public boolean isRelease() {
+		if( LIFECYCLESTAGE_TYPE == DBEnumLifecycleStageType.RELEASE )
+			return( true );
+		return( false );
+	}
+
+	public boolean isDeploy() {
+		if( LIFECYCLESTAGE_TYPE == DBEnumLifecycleStageType.DEPLOYMENT )
+			return( true );
+		return( false );
+	}
+
+	public int getDuration() {
+		if( DAYS < 0 )
+			return( 0 );
+		return( DAYS );
+	}
+
+	public boolean isUnlimited() {
+		if( DAYS < 0 )
+			return( true );
+		return( false );
+	}
+	
+}
