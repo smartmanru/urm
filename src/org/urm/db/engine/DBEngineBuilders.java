6302b37931a4f7c6d3eafef7e726337a895596c7
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2017-12-01 19:55:36 +0000

    fix

---
+++ src/org/urm/db/engine/DBEngineBuilders.java
@@ -0,0 +1,20 @@
+package org.urm.db.engine;
+
+import org.urm.meta.EngineLoader;
+import org.urm.meta.engine.EngineBuilders;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+public class DBEngineBuilders {
+
+	public static void importxml( EngineLoader loader , EngineBuilders builders , Node node ) throws Exception {
+	}
+	
+	public static void loaddb( EngineLoader loader , EngineBuilders builders ) throws Exception {
+	}
+	
+	public static void exportxml( EngineLoader loader , EngineBuilders builders , Document doc , Element root ) throws Exception {
+	}
+	
+}
---
+++ src/org/urm/db/engine/DBEngineMirrors.java
@@ -0,0 +1,20 @@
+package org.urm.db.engine;
+
+import org.urm.meta.EngineLoader;
+import org.urm.meta.engine.EngineMirrors;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+public class DBEngineMirrors {
+
+	public static void importxml( EngineLoader loader , EngineMirrors mirrors , Node node ) throws Exception {
+	}
+	
+	public static void loaddb( EngineLoader loader , EngineMirrors mirrors ) throws Exception {
+	}
+	
+	public static void exportxml( EngineLoader loader , EngineMirrors mirrors , Document doc , Element root ) throws Exception {
+	}
+	
+}
