93203831d005f6978f58b1194eac8cd0c5107aab
builder <builder@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2017-12-13 16:52:25 +0000

    fix

---
+++ install/database/101/install.sql
@@ -0,0 +1,3 @@
+﻿insert into main.urm_object_entity ( param_object_id , paramentity_type , custom , use_props , app_table , id_field , object_type , meta_object_id , meta_objectversion_type , data_objectversion_type,  version ) values ( 2 , 111 , 'yes' , 'yes' , null , null , 1 , 2 , 2 , 2 , 101 );
+insert into main.urm_object_entity ( param_object_id , paramentity_type , custom , use_props , app_table , id_field , object_type , meta_object_id , meta_objectversion_type , data_objectversion_type,  version ) values ( 2 , 112 , 'yes' , 'yes' , null , null , 1 , 2 , 2 , 2 , 101 );
+commit;
