36b5685e21b93c4e7b733d606eb4be1d2b136abb
builder <builder@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2017-12-19 17:59:43 +0000

    fix

--- master/database/sql/postgres/linux/checkconnect.sh
+++ master/database/sql/postgres/linux/checkconnect.sh
@@ -2,11 +2,17 @@
 # params:
 # URMDB_USER
 # URMDB_PWD
-# URMDB_DBHOST
+# URMDB_DBHOST (host[:port])
 # URMDB_DBNAME
 
 export PGPASSWORD="$URMDB_PWD"
-VALUE=`echo "select 'value=ok' as x;" | psql -d $URMDB_DBNAME -h $URMDB_DBHOST -U $URMDB_USER`
+XHOST=${URMDB_DBHOST%:*}
+XPORT=
+if [ "$XHOST" != "$URMDB_DBHOST" ]; then
+	XPORT=" -p ${URMDB_DBHOST#*:}"
+fi
+
+VALUE=`echo "select 'value=ok' as x;" | psql -d $URMDB_DBNAME -h $URMDB_DBHOST $XPORT -U $URMDB_USER`
 
 if [[ "$VALUE" =~ "value=ok" ]]; then
 	exit 0
