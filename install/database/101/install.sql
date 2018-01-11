c23c1ccd1afee718fcd99d3f72df10359603351b
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2018-01-11 09:36:14 +0000

    base status

--- install/database/101/install.sql
+++
@@ -1,5 +0,0 @@
-﻿insert into main.urm_object_entity ( param_object_id , paramentity_type , custom , use_props , app_table , id_field , object_type , meta_object_id , meta_objectversion_type , data_objectversion_type,  version ) values ( 3 , 111 , 'yes' , 'yes' , null , null , 1 , 2 , 2 , 2 , 101 );
-insert into main.urm_object_entity ( param_object_id , paramentity_type , custom , use_props , app_table , id_field , object_type , meta_object_id , meta_objectversion_type , data_objectversion_type,  version ) values ( 3 , 112 , 'yes' , 'yes' , null , null , 1 , 2 , 2 , 2 , 101 );
-update main.urm_object_version set version = 101 where owner_object_id = 1;
-update main.urm_object_type set name = 'local' where category = 512 and item = 3;
-commit;
