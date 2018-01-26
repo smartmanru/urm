e2e72ef97051066e2edcfd8722a0e222715773fd
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2018-01-26 11:29:59 +0000

    fix

---
+++ src/org/urm/meta/env/MetaEnvServerPrepareApp.java
@@ -0,0 +1,69 @@
+package org.urm.meta.env;
+
+import org.urm.action.ActionBase;
+import org.urm.engine.properties.PropertyController;
+import org.urm.meta.product.Meta;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+public class MetaEnvServerPrepareApp extends PropertyController {
+
+	protected Meta meta;
+	public MetaEnvServerBase base;
+
+	public String APP;
+	
+	public static String PROPERTY_APP = "app";
+	
+	public MetaEnvServerPrepareApp( Meta meta , MetaEnvServerBase base ) {
+		super( base , "prepare" );
+		this.meta = meta;
+		this.base = base;
+	}
+	
+	@Override
+	public String getName() {
+		return( APP );
+	}
+	
+	@Override
+	public boolean isValid() {
+		if( super.isLoadFailed() )
+			return( false );
+		return( true );
+	}
+	
+	@Override
+	public void scatterProperties( ActionBase action ) throws Exception {
+		APP = super.getStringPropertyRequired( action , PROPERTY_APP );
+		super.finishRawProperties();
+	}
+
+	public MetaEnvServerPrepareApp copy( ActionBase action , Meta meta , MetaEnvServerBase base ) throws Exception {
+		MetaEnvServerPrepareApp r = new MetaEnvServerPrepareApp( meta , base );
+		r.initCopyStarted( this , base.getProperties() );
+		r.scatterProperties( action );
+		r.resolveLinks( action );
+		r.initFinished();
+		return( r );
+	}
+	
+	public void resolveLinks( ActionBase action ) throws Exception {
+	}
+	
+	public void load( ActionBase action , Node node ) throws Exception {
+		if( !super.initCreateStarted( base.getProperties() ) )
+			return;
+
+		super.loadFromNodeAttributes( action , node , false );
+		scatterProperties( action );
+		super.loadFromNodeElements( action , node , true );
+		super.resolveRawProperties();
+	}
+	
+	public void save( ActionBase action , Document doc , Element root ) throws Exception {
+		super.saveSplit( doc , root );
+	}
+
+}
---
+++ src/org/urm/meta/env/MetaMonitoringItem.java
@@ -0,0 +1,74 @@
+package org.urm.meta.env;
+
+import org.urm.action.ActionBase;
+import org.urm.common.Common;
+import org.urm.common.ConfReader;
+import org.urm.db.core.DBEnums.*;
+import org.urm.meta.product.Meta;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+public class MetaMonitoringItem {
+
+	public Meta meta;
+	public MetaMonitoringTarget target;
+	
+	public int ID;
+	public DBEnumMonItemType MONITEM_TYPE;
+	public String URL;
+	public String WSDATA;
+	public String WSCHECK;
+	public int PV;
+
+	public MetaMonitoringItem( Meta meta , MetaMonitoringTarget target ) {
+		this.meta = meta; 
+		this.target = target;
+	}
+
+	public MetaMonitoringItem copy( Meta rmeta , MetaMonitoringTarget rtarget ) {
+		MetaMonitoringItem r = new MetaMonitoringItem( rmeta , rtarget );
+		r.ID = ID;
+		r.MONITEM_TYPE = MONITEM_TYPE;
+		r.URL = URL;
+		r.WSDATA = WSDATA;
+		r.WSCHECK = WSCHECK;
+		r.PV = PV;
+		return( r );
+	}		
+	
+	private String getNodeSubTree( ActionBase action , Node node , String name ) throws Exception {
+		Node parent = ConfReader.xmlGetFirstChild( node , name );
+		if( parent == null )
+			return( null );
+		
+		Node content = parent.getFirstChild();
+		if( content == null )
+			return( null );
+		
+		return( ConfReader.getNodeSubTree( content ) );
+	}
+
+	public void loadUrl( ActionBase action , Node node ) throws Exception {
+		URL = ConfReader.getRequiredAttrValue( node , "url" );
+		WSDATA = "";
+		WSCHECK = "";
+	}
+
+	public void loadWS( ActionBase action , Node node ) throws Exception {
+		URL = ConfReader.getRequiredAttrValue( node , "url" );
+		WSDATA = getNodeSubTree( action , node , "wsdata" );
+		WSCHECK = getNodeSubTree( action , node , "wscheck" );
+	}
+
+	public void save( ActionBase action , Document doc , Element root ) throws Exception {
+		if( MONITEM_TYPE == DBEnumMonItemType.CHECKURL )
+			Common.xmlSetElementAttr( doc , root , "url" , URL );
+		else
+		if( MONITEM_TYPE == DBEnumMonItemType.CHECKWS ) {
+			Common.xmlSetElementAttr( doc , root , "wsdata" , WSDATA );
+			Common.xmlSetElementAttr( doc , root , "wscheck" , WSCHECK );
+		}
+	}
+	
+}
