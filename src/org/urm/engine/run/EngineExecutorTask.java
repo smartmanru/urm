30f167ab9aeae693b6623266cae1ce551feece3c
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2018-03-21 16:47:28 +0000

    fix bug

---
+++ src/org/urm/engine/run/EngineExecutorTask.java
@@ -0,0 +1,44 @@
+package org.urm.engine.run;
+
+abstract public class EngineExecutorTask {
+
+	abstract public void execute() throws Exception;
+	
+	public String name;
+	public EngineExecutorThread thread;
+	public boolean runFailed;
+	public Throwable runException;
+	public int iteration;
+	
+	protected EngineExecutorTask( String name ) {
+		this.name = name;
+		runFailed = false;
+		iteration = 0;
+	}
+
+	public void setThread( EngineExecutorThread thread ) {
+		this.thread = thread;
+	}
+
+	public void start() {
+		iteration++;
+		runFailed = false;
+		runException = null;
+	}
+	
+	public boolean isRunning() {
+		if( thread == null )
+			return( false );
+		return( thread.isRunning() );
+	}
+
+	public void finishSuccessful() {
+		runFailed = false;
+	}
+	
+	public void finishFailed( Throwable e ) {
+		runFailed = true;
+		runException = e;
+	}
+	
+}
