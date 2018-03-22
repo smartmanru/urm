cf6f30786720d268b8e28aa8ff00189eaa4d58bb
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2018-03-22 16:35:01 +0000

    add releases

---
+++ src/org/urm/action/release/ActionCreateMaster.java
@@ -0,0 +1,33 @@
+package org.urm.action.release;
+
+import org.urm.action.ActionBase;
+import org.urm.engine.dist.Dist;
+import org.urm.engine.dist.DistRepository;
+import org.urm.engine.status.ScopeState;
+import org.urm.engine.status.ScopeState.SCOPESTATE;
+import org.urm.meta.product.Meta;
+
+public class ActionCreateMaster extends ActionBase { 
+
+	public Meta meta;
+	public String RELEASEVER;
+	public Dist dist;
+	public boolean copy;
+	
+	public ActionCreateMaster( ActionBase action , String stream , Meta meta , String RELEASEVER , boolean copy ) {
+		super( action , stream , "Create production coverage distributive" );
+		this.meta = meta;
+		this.RELEASEVER = RELEASEVER;
+		this.copy = copy;
+	}
+
+	@Override protected SCOPESTATE executeSimple( ScopeState state ) throws Exception {
+		DistRepository repo = meta.getDistRepository();
+		if( copy )
+			dist = repo.createMasterCopy( this , RELEASEVER );
+		else
+			dist = repo.createMasterInitial( this , RELEASEVER );
+		return( SCOPESTATE.RunSuccess );
+	}
+	
+}
