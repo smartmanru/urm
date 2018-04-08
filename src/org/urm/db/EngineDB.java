fca6892e89015d9923959d9277ca40e234b6b655
builder <builder@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2018-04-08 18:59:45 +0000

    releases

--- src/org/urm/db/EngineDB.java
+++ src/org/urm/db/EngineDB.java
@@ -24,7 +24,7 @@ public class EngineDB {
 	private Engine engine;
 	
 	private PGConnectionPoolDataSource pool;
-	public static int APP_VERSION = 1227;
+	public static int APP_VERSION = 1228;
 	
 	public EngineDB( Engine engine ) {
 		this.engine = engine;
