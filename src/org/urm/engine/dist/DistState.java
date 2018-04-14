8052a0249d47c607a849645af110d5b064392928
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2018-04-14 08:37:26 +0000

    fix bugs

--- src/org/urm/engine/dist/DistState.java
+++ src/org/urm/engine/dist/DistState.java
@@ -303,11 +303,11 @@ public class DistState {
 		if( state != DISTSTATE.DIRTY ) {
 			String dataHashCurrent = getDataHashValue( action );
 			if( dataHashCurrent.equals( dataHash ) == false )
-				action.exit0( _Error.DistributiveHashDiffers0 , "distributive is not ready for use, data hash value differs from declared (" + dataHashCurrent + ")" );
+				action.exit0( _Error.DistributiveHashDiffers0 , "distributive is not ready for use, data hash value differs from declared (" + dataHash + "/" + dataHashCurrent + ")" );
 			
 			String metaHashCurrent = getMetaHashValue( action );
 			if( metaHashCurrent.equals( metaHash ) == false )
-				action.exit0( _Error.DistributiveHashDiffers0 , "distributive is not ready for use, hash value differs from declared (" + metaHashCurrent + ")" );
+				action.exit0( _Error.DistributiveHashDiffers0 , "distributive is not ready for use, hash value differs from declared (" + metaHash + "/" + metaHashCurrent + ")" );
 		}
 	}
 
