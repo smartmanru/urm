4cb00e927c6a5b295acf707e299a99893f92e2fd
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2018-04-22 20:47:30 +0000

    add revisions

---
+++ src/org/urm/engine/transaction/TransactionMetadataEnv.java
@@ -0,0 +1,11 @@
+package org.urm.engine.transaction;
+
+import org.urm.engine.transaction.TransactionMetadata.CHANGETYPE;
+import org.urm.meta.env.MetaEnv;
+
+public class TransactionMetadataEnv {
+
+	public MetaEnv env;
+	public CHANGETYPE envType;
+	
+}
