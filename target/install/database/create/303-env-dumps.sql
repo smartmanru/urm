732fc74be9f8bd136b0eff5032ef9248c870b2db
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2018-05-04 12:55:54 +0000

    release 1.2.4

---
+++ target/install/database/create/303-env-dumps.sql
@@ -0,0 +1,44 @@
+﻿
+CREATE TABLE main.urm_env_dbdump (
+                dump_id INTEGER NOT NULL,
+                env_id INTEGER NOT NULL,
+                name VARCHAR(30) NOT NULL,
+                xdesc VARCHAR,
+                server_id INTEGER NOT NULL,
+                modeexport BOOLEAN NOT NULL,
+                dataset VARCHAR NOT NULL,
+                owntableset BOOLEAN NOT NULL,
+                dumpdir VARCHAR NOT NULL,
+                remote_setdbenv VARCHAR,
+                database_datapumpdir VARCHAR,
+                postrefresh VARCHAR,
+                schedule VARCHAR,
+                usestandby BOOLEAN NOT NULL,
+                usenfs BOOLEAN NOT NULL,
+                offline BOOLEAN NOT NULL,
+                ev INTEGER NOT NULL,
+                CONSTRAINT urm_env_dbdump_pk PRIMARY KEY (dump_id)
+);
+COMMENT ON TABLE main.urm_env_dbdump IS 'Database dump';
+
+
+CREATE TABLE main.urm_env_tablemask (
+                tablemask_id INTEGER NOT NULL,
+                env_id INTEGER NOT NULL,
+                dump_id INTEGER NOT NULL,
+                modeinclude BOOLEAN NOT NULL,
+                schema_fkid INTEGER,
+                schema_fkname VARCHAR(30),
+                tablemask VARCHAR NOT NULL,
+                ev INTEGER NOT NULL,
+                CONSTRAINT urm_env_tablemask_pk PRIMARY KEY (tablemask_id)
+);
+COMMENT ON TABLE main.urm_env_tablemask IS 'Database dump table set table mask';
+
+
+ALTER TABLE main.urm_env_tablemask ADD CONSTRAINT urm_env_dbdump_urm_env_tablemask_fk
+FOREIGN KEY (dump_id)
+REFERENCES main.urm_env_dbdump (dump_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
