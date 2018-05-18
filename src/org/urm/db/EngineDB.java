7bc643975997f2a60b2d746999fbaf555e382a22
admin <admin@98f3f8d0-d3d2-46e7-9302-4021e2e13586>
2018-05-18 16:49:19 +0000

    add security

--- src/org/urm/db/EngineDB.java
+++ src/org/urm/db/EngineDB.java
@@ -27,7 +27,7 @@ public class EngineDB {
 	private Engine engine;
 	
 	private PGConnectionPoolDataSource pool;
-	public static int APP_VERSION = 0x01020403;
+	public static int APP_VERSION = 0x01020404;
 	
 	public EngineDB( Engine engine ) {
 		this.engine = engine;
